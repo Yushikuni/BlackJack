c457565de5a9e9a5b15b597487dda2175378e023
Kvetuse Husakova <42646031+Yushikuni@users.noreply.github.com>
2022-03-18 17:45:58 +0100

    add code
    tranclate from original file: https://github.com/Yushikuni/Karetnich_21-OkoBere

---
+++ Karetnich21/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karetních_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random nahodnaCisla = new Random();
+            int kartyHrace = nahodnaCisla.Next(1, 12);
+            int kartyPC = nahodnaCisla.Next(1, 12);
+        DalsiKarta:
+            Console.WriteLine("Do you wan another card?? yes/no You have: " + kartyHrace);
+            string volba = Console.ReadLine();
+            if (volba == "yes")
+            {
+                kartyHrace += nahodnaCisla.Next(1, 12);
+                if (kartyPC < 15)
+                {
+                    kartyPC += nahodnaCisla.Next(1, 12);
+                }
+                goto DalsiKarta;
+            }
+            else if (volba == "no")
+            {
+
+                /*System.Threading.Thread.Sleep(1000);*/
+                if (kartyHrace <= 21 && (kartyPC > 21 || kartyPC < kartyHrace))
+                {
+                    Console.WriteLine("You won!! AI had " + kartyPC + " points!");
+                }
+                else if (kartyHrace <= 21 && (kartyPC > 21 || kartyPC > kartyHrace))
+                {
+                    Console.WriteLine("You lost!! AI had " + kartyPC + " points!");
+                }
+                else if (kartyPC > 21 && kartyHrace > 21)
+                {
+                    Console.WriteLine("Both players are lost!! AI had " + kartyPC + " points!");
+                }
+
+                else if (kartyHrace == kartyPC)
+                {
+                    Console.WriteLine("No one wins!!");
+                }
+                else if (kartyHrace > 21)
+                {
+                    Console.WriteLine("You lost bacause you already have moore than 21! O_o");
+                }
+                System.Threading.Thread.Sleep(5000);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect input!!!");
+                goto DalsiKarta;
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,12 +1,19 @@
-# BlackJack
+# BlackJack in C#
+
+### Images:
+
+![NazevHry](https://user-images.githubusercontent.com/42646031/151656263-09b8491a-2e93-4876-ae53-47e898ad736f.png)
+
+![JedenKonecHry](https://user-images.githubusercontent.com/42646031/151656262-d9492bd5-e0b7-4889-a107-53fb30aff61e.png)
+
+![DruhyKonecHry](https://user-images.githubusercontent.com/42646031/151656261-d7f3ba7b-bb65-46cb-b111-87560988e1a0.png)
+
+### EN:
+
 Try to obtain 21 and win the game!
 
-Blackjack have a basic random number generator that uses a counter for previous card
+Black Jack have a basic random number generator that uses a counter for previous card
 
-![image](https://user-images.githubusercontent.com/42646031/159046069-9dab3c81-9a8f-4f1a-b677-4dbd994c4b3c.png)
+### Karetnich_21-OkoBere
 
-![image](https://user-images.githubusercontent.com/42646031/159044839-1290f856-98e2-4ab5-8cd5-eefc9e6548bc.png)
-
-![image](https://user-images.githubusercontent.com/42646031/159045184-3ffb3740-6395-4dfe-a164-77b5d6293e09.png)
-
-![image](https://user-images.githubusercontent.com/42646031/159045737-5b367714-7d6b-4612-8a60-9b3e822a0bf6.png)
+Karetnich 21 nebo zname jako oko bere, je hra z knihy začínáme programovat hry v jazyce C#
